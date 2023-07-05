11afc80fd04fdfc09df2f1adf3f379ffeaaba66b
Danrllei Miranda <danmiranda14@gmail.com>
2023-07-05 15:34:26 -0300

    requisitos 1, 2 e 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 function App() {
-  return (<p>Sistema Solar</p>);
+  return (
+    <main>
+      <Header />
+      <SolarSystem />
+      <Title headline="Planetas" />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+type TitleProp = {
+  headline: string
+};
+
+function Title({ headline } :TitleProp) {
+  return (<h2>{headline}</h2>
+  );
+}
+
+export default Title;
